97b9caf0330351556282ff888f0b2ada9088c5b3
Mitchell <83954323+Quitzchell@users.noreply.github.com>
2023-10-06 18:05:30 +0200

    refactor factory

--- app/factories/chatbot_factory.py
+++ app/factories/chatbot_factory.py
@@ -4,21 +4,34 @@ from app.chatbot import Chatbot
 from app.components.embeddings import Embeddings
 from app.components.language_model import LanguageModel
 from app.components.vector_store import VectorStore
+from app.utils.text_utils import TextExtractor
 
 
 class ChatbotFactory:
     @staticmethod
-    def create_chatbot(config_file_path):
-        with open(config_file_path, 'r') as file:
+    def create_chatbot(config_path: str):
+        with open(config_path, 'r') as file:
             config = json.load(file)
 
-        # Create language model instance
-        language_model = LanguageModel(config["language_model"]["model_name"], config["language_model"]["api_key"])
+        # Create embeddings
+        embeddings = Embeddings(
+            model_name=config['embeddings']['model_name']
+        )
 
-        # Create embeddings generator
-        embeddings = Embeddings(config['model_name'],)
+        # Create document for vector store
+        raw_text = TextExtractor.extract_text_from_pdf(pdf_path=config["vector_store"]["path"])
+        chunks = TextExtractor.chunk_text(raw_text=raw_text)
 
         # Create vector store instance
-        vector_store = VectorStore(config["vector_store"]["type"], config["vector_store"]["path"])
+        vector_store = VectorStore(
+            texts=chunks,
+            embeddings=embeddings.get_embedding
+        )
+
+        # Create language model instance
+        language_model = LanguageModel(
+            model_name=config["language_model"]["model_name"],
+            api_key=config["language_model"]["api_key"],
+        )
 
         return Chatbot(language_model, embeddings, vector_store)
