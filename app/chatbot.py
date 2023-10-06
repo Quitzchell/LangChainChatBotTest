570a94b7afb274d6fbfb530122eb3aacee147e7d
Mitchell <83954323+Quitzchell@users.noreply.github.com>
2023-10-06 18:06:01 +0200

    refactor chatbot model

--- app/chatbot.py
+++ app/chatbot.py
@@ -1,9 +1,24 @@
+from app.components.embeddings import Embeddings
+from app.components.language_model import LanguageModel
+from app.components.vector_store import VectorStore
+
+
 class Chatbot:
-    def __init__(self, language_model, embeddings, vector_store):
+    def __init__(self, language_model: LanguageModel, embeddings: Embeddings, vector_store: VectorStore):
         self.language_model = language_model
         self.embeddings = embeddings
         self.vector_store = vector_store
+        self.context = {'chat_history': []}
 
     def answer_question(self, question):
         # Logic to answer a question using the language model and vector store
-        pass
+        response = self.language_model.generate_response(vectorstore=self.vector_store)
+
+        # Add chat history to context
+        self.context['chat_history'].append(response['chat_history'])
+
+        print(response)
+
+        # Process the response and return an answer
+        # Modify this part based on how you handle the response
+        return response
