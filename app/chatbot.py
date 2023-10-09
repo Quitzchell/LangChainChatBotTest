7c9d2f630b313570d8d20ff8b0be7d72cf053699
Mitchell <83954323+Quitzchell@users.noreply.github.com>
2023-10-10 00:02:41 +0200

    inject abstract base class instead of concrete class into chatbot.py

--- app/chatbot.py
+++ app/chatbot.py
@@ -1,12 +1,10 @@
 from app.components.embeddings.abstract.embeddings import Embeddings
-from app.components.language_models.huggingface_hub_language_model_strategy import HuggingFaceHubLanguageModelStrategy
-from app.components.language_models.openai_language_model_strategy import OpenAiLanguageModel
+from app.components.language_models.abstract.language_model import LanguageModel
 from app.components.vectorstores.faiss_from_text_strategy import VectorStore
 
 
 class Chatbot:
-    def __init__(self, language_model: OpenAiLanguageModel, embeddings: Embeddings,
-                 vector_store: VectorStore):
+    def __init__(self, language_model: LanguageModel, embeddings: Embeddings, vector_store: VectorStore):
         self.language_model = language_model
         self.embeddings = embeddings
         self.vector_store = vector_store
