490ef138dfaa29cb2765cae0af93ac98209f7bb4
Mitchell <83954323+Quitzchell@users.noreply.github.com>
2023-10-10 00:48:29 +0200

    add return type hints

--- app/factories/chatbot_factory.py
+++ app/factories/chatbot_factory.py
@@ -1,13 +1,16 @@
 import json
 
 from app.chatbot import Chatbot
+from app.components.embeddings.abstract.embeddings import Embeddings
+from app.components.language_models.abstract.language_model import LanguageModel
+from app.components.vectorstores.abstract.vectorStore import VectorStore
 from app.utils.embedding_selector import EmbeddingsSelector
 from app.utils.language_model_selector import LanguageModelSelector
 from app.utils.vectorstore_selector import VectorStoreSelector
 
 
 class ChatbotFactory:
-    def create_chatbot(self, config_path: str):
+    def create_chatbot(self, config_path: str) -> Chatbot:
         config = self.load_config(config_path)
         embeddings = self.create_embeddings(config)
         vector_store = self.create_vector_store(config, embeddings)
@@ -19,12 +22,12 @@ class ChatbotFactory:
         with open(config_path, 'r') as file:
             return json.load(file)
 
-    def create_embeddings(self, config):
+    def create_embeddings(self, config) -> Embeddings:
         embeddings_strategy = config['embeddings']['strategy']
         embeddings_class = EmbeddingsSelector().get_embeddings_class(embeddings_strategy)
         return embeddings_class(model_name=config['embeddings']['model_name'])
 
-    def create_vector_store(self, config, embeddings):
+    def create_vector_store(self, config, embeddings) -> VectorStore:
         if 'local_path' in config:
             path = config['vector_store']['local_path']
             vector_store_strategy = config['vector_store']['local_strategy']
@@ -35,7 +38,7 @@ class ChatbotFactory:
         vector_store_class = VectorStoreSelector().get_vectorstore(vector_store_strategy)
         return vector_store_class(path=path, embeddings=embeddings.get_embedding())
 
-    def create_language_model(self, config):
+    def create_language_model(self, config) -> LanguageModel:
         language_model_strategy = config['language_model']['strategy']
         language_model_class = LanguageModelSelector().get_language_model_class(language_model_strategy)
         return language_model_class(model_name=config['language_model']['model_name'])
