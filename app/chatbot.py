1abdea3609231f7241e890c9b5fedba060ed2b54
Mitchell <83954323+Quitzchell@users.noreply.github.com>
2023-10-10 00:02:10 +0200

    remove context from chatbot.py

--- app/chatbot.py
+++ app/chatbot.py
@@ -10,16 +10,13 @@ class Chatbot:
         self.language_model = language_model
         self.embeddings = embeddings
         self.vector_store = vector_store
-        self.context = {'chat_history': []}
 
     def answer_question(self, question):
         # Logic to answer a question using the language model and vector store
-        response = self.language_model.generate_response(question=question, vectorstore=self.vector_store)
+        response = self.language_model.generate_response(question=question, vector_store=self.vector_store)
 
         # todo: find out if this is useful for persisting chat history during a chat session
         self.context = response['chat_history']
         # self.context['chat_history'].append(response['chat_history'])
 
-        # Process the response and return an answer
-        # Modify this part based on how you handle the response
         return response['answer']
