1977afbe8f94116abca1e2b994d44012078dbf1c
Mitchell <83954323+Quitzchell@users.noreply.github.com>
2023-10-10 00:03:06 +0200

    add todos to chatbot.py

--- app/chatbot.py
+++ app/chatbot.py
@@ -6,15 +6,13 @@ from app.components.vectorstores.faiss_from_text_strategy import VectorStore
 class Chatbot:
     def __init__(self, language_model: LanguageModel, embeddings: Embeddings, vector_store: VectorStore):
         self.language_model = language_model
-        self.embeddings = embeddings
+        self.embeddings = embeddings # todo: might not need to inject embeddings
         self.vector_store = vector_store
 
     def answer_question(self, question):
         # Logic to answer a question using the language model and vector store
         response = self.language_model.generate_response(question=question, vector_store=self.vector_store)
 
-        # todo: find out if this is useful for persisting chat history during a chat session
-        self.context = response['chat_history']
-        # self.context['chat_history'].append(response['chat_history'])
+        # todo: find out if we need to implement something to persist history
 
         return response['answer']
