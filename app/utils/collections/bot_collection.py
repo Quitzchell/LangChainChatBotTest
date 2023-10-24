744f334c62e7e469e00680333164ee733ae0b728
Mitchell <83954323+Quitzchell@users.noreply.github.com>
2023-10-24 22:58:03 +0200

    refactor bot_collection

--- app/utils/collections/bot_collection.py
+++ app/utils/collections/bot_collection.py
@@ -1,14 +1,15 @@
 class BotCollection:
     bot_configurations = {
         '1': 'flan_t5-xxl_instructor-xl_faiss.json',
-        '2': 'open_ai_faiss.json'
+        '2': 'open_ai_all-minilm-l6-v2_faiss.json'
     }
 
     def select_bot(self):
         while True:
             print("Select a bot:")
             for index, bot_name in self.bot_configurations.items():
-                print(f"{index}: {bot_name}")
+                format_bot_name = bot_name.replace('_', ' ').rsplit('.', 1)[0]
+                print(f"{index}: {format_bot_name}")
 
             choice = input("Enter the index of the bot you want to use: ")
 
